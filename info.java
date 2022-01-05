4f76541d9d0284ed34d78a599818602ab6499eda
Pooja <poojaekbote97@gmail.com>
2022-01-05 18:57:27 +0530

    created info  file

---
+++ info.java
@@ -0,0 +1,8 @@
+public class info {
+
+    public static void main(String[] args) {
+        System.out.println("My name is pooja");
+        System.out.println("This my information file");
+
+    }
+}
